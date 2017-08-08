cd8b8a8db9c06ad864ca7c14c81f46140c437e7e
toratios <max.pagel@uni-weimar.de>
2017-08-08 15:35:34 +0200

    sdfloader working with box and sphere

--- framework/sdfloader.cpp
+++ framework/sdfloader.cpp
@@ -1,42 +1,105 @@
-/*#include "sdfloader.hpp"
+#include "sdfloader.hpp"
 
 
 Scene SDFloader::sdfLoad(std::string const& inputFile)
 {
 	Scene scene;
 
-	std::fstream input;
-	input.open("material.txt");
+	std::fstream inputfile;
+	inputfile.open("material.txt");
 
-	if(input.is_open())
+	if(inputfile.is_open())
 	{
 		std::string line;
 		std::string word;
 		
-		while(std::getline(input, line))
+		while(std::getline(inputfile, line))
 		{	
+			std::stringstream stream;
+
 			stream << line; 
-			stream >> word;	
+			stream >> word;
+
 			if(word == "define")
 			{
 				stream >> word;
 
 				if(word == "material")
-				{
-					Material mat;
-						
-					stream >> mat.name_;
-					stream >> mat.ka_.r;
-					stream >> mat.ka_.g;
-					stream >> mat.ka_.b;
-					stream >> mat.kd_.r;
-					stream >> mat.kd_.g;
-					stream >> mat.kd_.b;
-					stream >> mat.ks_.r;
-					stream >> mat.ks_.g;
-					stream >> mat.ks_.b;
+				{	
+					std::string matname;
+					Color ka;
+					Color kd;
+					Color ks;
+					float m;
 
-					//vecMaterial_.push_back(mat);
+					stream >> matname;
+
+					stream >> ka.r;
+					stream >> ka.g;
+					stream >> ka.b;
+
+					stream >> kd.r;
+					stream >> kd.g;
+					stream >> kd.b;
+
+					stream >> ks.r;
+					stream >> ks.g;
+					stream >> ks.b;
+
+					stream >> m;
+
+
+					std::shared_ptr<Material> material = std::make_shared<Material>(matname, ka, kd, ks, m);
+					scene.materials_.insert(std::pair<std::string, std::shared_ptr<Material>>(matname, material));
+				}
+
+				else if(word == "shape")
+				{
+					stream >> word;
+
+					if(word == "box")
+					{
+						std::string boxname;
+						glm::vec3 min;
+						glm::vec3 max;
+						std::string matname;
+
+						stream >> boxname;
+
+						stream >> min.x;
+						stream >> min.y;
+						stream >> min.z;
+
+						stream >> max.x;
+						stream >> max.y;
+						stream >> max.z;
+
+						stream >> matname;
+
+						std::shared_ptr<Material> material = (scene.materials_.find(matname)->second);
+						std::shared_ptr<Shape> box = std::make_shared<Box>(min, max , material, boxname);
+					}
+
+					else if (word == "sphere")
+					{
+						std::string spherename;
+						glm::vec3 center;
+						float r;
+						std::string matname;
+
+						stream >> spherename;
+
+						stream >> center.x;
+						stream >> center.y;
+						stream >> center.z;
+
+						stream >> r;
+
+						stream >> matname;
+
+						std::shared_ptr<Material> material = (scene.materials_.find(matname)->second);
+						std::shared_ptr<Shape> sphere = std::make_shared<Sphere>(center, r, material, spherename);
+					}
 				}
 			}
 		}
@@ -47,4 +110,3 @@ Scene SDFloader::sdfLoad(std::string const& inputFile)
 	}
 }
 
-*/
--- framework/sdfloader.hpp
+++ framework/sdfloader.hpp
@@ -1,11 +1,14 @@
-/*#ifndef SDF_LOADER_HPP
+#ifndef SDF_LOADER_HPP
 #define SDF_LOADER_HPP
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
 #include "material.hpp"
+#include "box.hpp"
+#include "sphere.hpp"
 #include "scene.hpp"
 
 struct SDFloader{
@@ -17,4 +20,3 @@ struct SDFloader{
 };
 
 #endif
-*/
